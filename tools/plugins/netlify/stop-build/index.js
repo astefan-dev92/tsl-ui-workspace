2bce6cf35c1259ef29599ec7605c2510003ec4b7
Alex Stefan <73831093+astefan-dev92@users.noreply.github.com>
2021-10-13 17:50:37 +0100

    Detect whether project actually changed

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,12 +1,29 @@
 module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = 'admin-experience';
-        const projectHasChanged = false;
+        const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+        const latestCommit = 'HEAD';
 
-        if (!projectHasChanged) {
+        const changed = hasProjectChanged(currentProject, lastDeployedCommit, latestCommit);
+
+        if (!changed) {
             utils.build.cancelBuild(
                 `Build was cancelled because ${currentProject} was not affect by the latest changes`
             );
         }
     }
 };
+
+const hasProjectChanged = (currentProject, fromHash, toHash) => {
+    const execSync = require('child_process').execSync;
+    const getAffected = `npm --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+    const output = execSync(getAffected).toString();
+
+    const changedProjects = JSON.parse(output).projects;
+
+    if (changedProjects.find((project) => project === currentProject)) {
+        return true;
+    }
+
+    return false;
+}
